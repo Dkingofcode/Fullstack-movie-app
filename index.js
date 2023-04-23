8ee5610f1dde969781352dc95cf65d7a72f8fc90
Dkingofcode <doladepo128@gmail.com>
2023-04-23 14:56:09 +0100

    Installed express, cors and added .env to .gitignore

---
+++ index.js
@@ -0,0 +1,33 @@
+import app from './server.js';
+import mongodb from "mongodb";
+import ReviesDAO from "./dao/reviewsDAO";
+
+
+const  MongoClient = mongodb.MongoClient
+const mongo_username = process.env["MONGO_USERNAME"]
+const mongo_password = process.env["MONGO_PASSWORD"]
+const uri = `mongodb+srv://kdavidmongoose2001A:<password>@cluster0.xdmae.mongodb.net/test` 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
